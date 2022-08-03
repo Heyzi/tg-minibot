5b935ab67c995dc9405f53cb2d0c01929a56d2ab
Alexandr Platonov <planol@me.co>
2022-08-03 11:34:24 +0300

    /add and /remove released

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def isAdmin(message):
 def urlValidator(message, site):
         #если аргументов нет
     if not site:
-        bot.reply_to(message, "No url added")
+        bot.reply_to(message, "you need to specify the address after command")
     else:
     #проверяем аргумент на валидность
         pattern = re.compile("^((?:\/\/)?)?\w+(?:[-.]\w+)+(?:\/)*$")
@@ -22,16 +22,26 @@ def urlValidator(message, site):
             return(site[0])
         else:
             #аргумент невалиден
-            bot.reply_to(message, "URL format incorrect")
+            bot.reply_to(message, "address is not valid")
             return False
+
+def is_file_empty(file_name):
+    try:
+        with open(file_name, 'r') as read_obj:
+         one_char = read_obj.read(1)
+         if one_char:
+            return False
+    except OSError:
+        bot.reply_to(message, "unable to open a list file")
+    return True
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     if isAdmin(message):
-        bot.reply_to(message, """Примеры команд: 
-"/add google.com"       - добавить google.com
-"/remove google.com"    - удалить google.com
-"/list                  - вывести список хостов
+        bot.reply_to(message, """available commands: 
+"/add google.com"       - add google.com to unblock list
+"/remove google.com"    - remove google.com from unblock list
+"/list                  - list unblocked hosts
         """)
 
 @bot.message_handler(func=lambda message: message.text is not None and '/start' not in message.text)
@@ -40,29 +50,58 @@ def main(message):
         if message.text.startswith('/add'):
             site = extract_arg(message.text)
             if urlValidator(message, site):
-                #дописать часть на добавление
-                bot.reply_to(message, "URL added: " + site[0])
+                #добавляем наш сайт в файл
+                try:
+                    with open(cfg.hostlist, 'a+') as file:
+                        file.seek(0)
+                        content = file.read()
+                        if site[0] not in content:
+                            file.write(site[0] + '\n')                           
+                            bot.reply_to(message, "address: " + site[0] + " added")
+                        else:
+                            bot.reply_to(message, "address " + site[0] + " is already added")
+                    file.close()
+                except OSError:
+                    bot.reply_to(message, "unable to open a list file")
+
 
         elif message.text.startswith('/remove'):
             site = extract_arg(message.text)
             if urlValidator(message, site):
-                #дописать часть на удаление
-                bot.reply_to(message, "URL removed: " + site[0])
-            
+                if not is_file_empty(cfg.hostlist):
+                    try:
+                        with open(cfg.hostlist, 'r+') as f:
+                            lines = f.readlines()
+                            f.close()
+                            with open(cfg.hostlist, 'w') as f:
+                                for line in lines:
+                                    if site[0] not in line:
+                                        f.write(line)
+                            f.close()  
+                            bot.reply_to(message, "Site " + site[0] + " removed")
+                    except OSError:
+                        bot.reply_to(message, "unable to open a list file")
+                else:
+                    bot.reply_to(message, "file is empty, nothing to delete")
+
+
         elif message.text.startswith('/list'):
             #выводим список хостов
-            try:
-                with open(cfg.hostlist, 'r') as f:
-                    hostslist = f.read()
-                    f.close()
-                    bot.reply_to(message, hostslist)
-            except OSError:
-                bot.reply_to(message, "Cant open host file")
+            if not is_file_empty(cfg.hostlist):
+                try:
+                    with open(cfg.hostlist, 'r') as f:
+                        hostslist = f.read()
+                        f.close()
+                        bot.reply_to(message, hostslist)
+                except OSError:
+                    bot.reply_to(message, "unable to open a list file")           
+            else:
+                bot.reply_to(message, "file is empty")
+
 
         elif message.text.startswith('/status'):
             #выводим список хостов
             bot.reply_to(message, "Надо придумать статусы на основные сервисы")
-
         else:
             bot.reply_to(message, "Unknown command")
 
